48d8320a0465aad8d25c15b2f6fb75713c433817
Gavin <gavinwale@u.boisestate.edu>
2022-09-09 23:11:59 -0600

    Updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,35 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import {bg} from "../images/frontpagebg.png";
+import bg from "../images/frontpagebg.png";
 import logo from "../images/airbnb.png";
+import {ConnectButton} from "web3uikit";
 
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="containerGradinet"></div> 
+      </div>
+      <div className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="logo"></img>
+        </div>
+        <div className="tabs">
+          <div className="selected">Places to Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div className="lrContainers">
+          <ConnectButton />
+
+        </div>
+      </div>
+      <div className="tabContent">
+        <div className="searchFields">
+        </div>
+      </div>
     </>
   );
 };
