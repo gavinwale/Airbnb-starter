d2b85d88b9969b17cadd00e3d126da20c21b5f5f
Gavin <gavinwale@u.boisestate.edu>
2022-09-09 23:43:20 -0600

    more

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,19 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import bg from "../images/frontpagebg.png";
 import logo from "../images/airbnb.png";
-import {ConnectButton} from "web3uikit";
+import {ConnectButton, Icon, Select, DatePicker, Input, Button} from "web3uikit";
+import { useState } from "react";
 
 const Home = () => {
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [destination, setDestination] = useState("New York");
+  const [guests, setGuests] = useState(2);
 
   return (
     <>
       <div className="container" style={{ backgroundImage: `url(${bg})` }}>
-        <div className="containerGradinet"></div> 
+        <div className="containerGradinet"></div>
       </div>
       <div className="topBanner">
         <div>
@@ -28,7 +33,79 @@ const Home = () => {
       </div>
       <div className="tabContent">
         <div className="searchFields">
+        <div className="inputs">
+          Location
+          <Select
+            defaultOptionIndex={0}
+            onChange={(data) => setDestination(data.label)}
+            options={[
+            {
+              id: "ny",
+              label: "New York"
+            },
+            {
+              id: "london",
+              label: "London"
+            },
+            {
+              id: "db",
+              label: "Dubai"
+            },
+            {
+              id: "la",
+              label: "Los Angeles"
+            },
+            ]}
+            />
         </div>
+        <div className="vl" />
+        <div className="inputs">
+          Check In
+          <DatePicker 
+            id="CheckIn"
+            onChange={(event) => setCheckIn(event.date)}
+          />
+        </div>
+        <div className="vl" />
+        <div className="inputs">
+          Check Out
+          <DatePicker 
+            id="CheckOut"
+            onChange={(event) => setCheckOut(event.date)}
+          />
+        </div>
+        <div className="vl" />
+        <div className="inputs">
+          Guests
+          <Input
+            value={2}
+            name="AddGuests"
+            type="number"
+            onChange={(event) => setGuests(Number(event.target.value))}  
+          />
+        </div>
+        <Link to={"/rentals"} state={{
+          destination: destination,
+          checkIn: checkIn,
+          checkOut: checkOut,
+          guests: guests
+        }}>
+        <div className="searchButton">
+          <Icon fill="#ffffff" size={24} svg="search" />
+        </div>
+        </Link>
+        </div>
+      </div>
+      <div className="randomLocation">
+        <div className="title">Feel Adventurous</div>
+        <div className="text">
+          Let us decide and discover new places to stay, live, work or just
+          relax.
+        </div>
+        <Button
+          text="Explore A Location"
+          onClick={() => console.log(checkOut)}
+          />
       </div>
     </>
   );
